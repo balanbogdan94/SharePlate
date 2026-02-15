2f1b0c2a3558fdbce79d330541d4334430d311b8
balanbogdan94-pc <balan.bogdan94@gmail.com>
2026-02-15 23:35:45 +0200

    Add frontend application (React + TanStack)

---
+++ frontend/src/integrations/tanstack-query/root-provider.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react'
+
+const queryClient = new QueryClient()
+
+export function getContext() {
+  return {
+    queryClient,
+  }
+}
+
+export default function TanStackQueryProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
