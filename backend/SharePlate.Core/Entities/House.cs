77b3724ad6e0fe8897bd3f9ab39dd426ac111ca0
balanbogdan94-pc <balan.bogdan94@gmail.com>
2026-02-24 23:59:49 +0200

    feat: Refactor House creation to auto-generate code and include owner ID

--- backend/SharePlate.Core/Entities/House.cs
+++ backend/SharePlate.Core/Entities/House.cs
@@ -1,22 +1,29 @@
+using SharePlate.Core.Enums;
+
 namespace SharePlate.Core.Entities;
 
 public sealed class House : BaseEntity
 {
     private House() { }
 
-    public static House Create(string name, string code)
+    public static House Create(string name, Guid ownerId)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(name);
-        ArgumentException.ThrowIfNullOrWhiteSpace(code);
 
-        return new House
+        var house = new House
         {
             Id = Guid.NewGuid(),
             Name = name,
-            Code = code,
+            Code = GenerateCode(),  // 👈 Auto-generated, no longer a param
+            IsPersonal = false,
+            OwnerId = ownerId,
             CreatedAt = DateTime.UtcNow,
             UpdatedAt = DateTime.UtcNow
         };
+
+        house.HouseMembers.Add(HouseMember.Create(house.Id, ownerId, HouseMemberRole.Owner));
+
+        return house;
     }
 
     public string Name { get; private set; } = string.Empty;
@@ -24,7 +31,20 @@ public sealed class House : BaseEntity
 
     public bool IsPersonal { get; private set; } = true;
 
+    public Guid OwnerId { get; private set; }
+
     public ICollection<HouseMember> HouseMembers { get; private set; } = new List<HouseMember>();
     public ICollection<MealPlan> MealPlans { get; private set; } = new List<MealPlan>();
     public ICollection<ShoppingItem> ShoppingItems { get; private set; } = new List<ShoppingItem>();
+
+    private static string GenerateCode()
+    {
+        const string chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+        var random = new Random();
+
+        var part1 = new string(Enumerable.Range(0, 4).Select(_ => chars[random.Next(chars.Length)]).ToArray());
+        var part2 = new string(Enumerable.Range(0, 4).Select(_ => chars[random.Next(chars.Length)]).ToArray());
+
+        return $"{part1}-{part2}";
+    }
 }
