806c4ca90ebd78a58ca94107e337880807b91a60
balanbogdan94-pc <balan.bogdan94@gmail.com>
2026-02-28 13:57:20 +0200

    feat: Add authentication persistence with refresh token support and user password management

--- backend/SharePlate.Infrastructure/Migrations/20260228110807_AddAuthPersistence.Designer.cs
+++ backend/SharePlate.Infrastructure/Migrations/20260228110807_AddAuthPersistence.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SharePlate.Infrastructure.Data;
@@ -11,9 +12,11 @@ using SharePlate.Infrastructure.Data;
 namespace SharePlate.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260228110807_AddAuthPersistence")]
+    partial class AddAuthPersistence
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
